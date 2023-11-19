cb1e61703725b92359c38a0a14c030f51ea067fc
neilteje <neilteje@gmail.com>
2023-11-18 21:20:42 -0500

    some stuffs

---
+++ app.py
@@ -0,0 +1,11 @@
+from multi import MultiApp
+import streamlit as st
+import pandas as pd
+st.write("""
+# My first app
+Hello *world!*
+""")
+app = MultiApp()
+st.set_page_config(layout="wide")
+def app0():
+    
